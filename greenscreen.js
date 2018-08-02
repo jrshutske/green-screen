07883f19feb6e80bfc65ca3f9e35b4bf2ac47ff8
jrshutske <jackshutske@gmail.com>
2018-08-02 13:31:34 -0500

    Added Watermark, added video with autoplay capabilities, converted avi to mp4,cleaned general code, and more formatting

--- greenscreen.js
+++ greenscreen.js
@@ -1,27 +1,22 @@
 $(document).ready(function(){
-
-
-
   setInterval(myImageTimer, 100000);
   myImageTimer();
-  function myImageTimer() {
 
+  function myImageTimer() {
     $("img").remove();
     $.getJSON("images.json", function(images) {
-        tlLength = Object.keys(images.topleft).length;
-       trLength = Object.keys(images.topright).length;
-       brLength = Object.keys(images.bottomright).length;
-       blLength = Object.keys(images.bottomleft).length;
-
-
-       setInterval(function() {
-         $('#topright > div:first')
-           .fadeOut(2000)
-           .next()
-           .fadeIn(1000)
-           .end()
-           .appendTo('#topright');
-       }, 5000);
+      tlLength = Object.keys(images.topleft).length;
+      trLength = Object.keys(images.topright).length;
+      brLength = Object.keys(images.bottomright).length;
+      blLength = Object.keys(images.bottomleft).length;
+      setInterval(function() {
+        $('#topright > div:first')
+        .fadeOut(2000)
+        .next()
+        .fadeIn(3500)
+        .end()
+        .appendTo('#topright');
+       }, 7000);
        for (var r = 1; r < trLength; r++) {
          var trimg = document.createElement('img');
          var trdiv = document.createElement('div');
@@ -34,12 +29,12 @@ $(document).ready(function(){
 
        setInterval(function() {
          $('#bottomleft > div:first')
-           .fadeOut(2000)
+           .fadeOut(1000)
            .next()
-           .fadeIn(1000)
+           .fadeIn(2000)
            .end()
            .appendTo('#bottomleft');
-       }, 5000);
+       }, 7000);
        for (var l = 1; l < blLength; l++) {
          var blimg = document.createElement('img');
          var bldiv = document.createElement('div');
@@ -50,27 +45,22 @@ $(document).ready(function(){
        }
        $("#bottomleft > div:gt(0)").hide();
 
-         var tlsource = document.createElement('source');
-         tlsource.src = '/images/topleft/' + images.topleft[1];
-         document.getElementById('myvideo').appendChild(tlsource);
-         tlsource.type = "video/mp4"
-
-
-
-
-    });
+       var tlsource1 = document.createElement('source');
+       tlsource1.src = '/images/topleft/' + images.topleft[1];
+       document.getElementById('myvideo1').appendChild(tlsource1);
+       tlsource1.type = "video/mp4";
+       console.log(tlsource1)
+     });
   }
 
-
-
-setInterval(function() {
-  $('#employees > #table:first')
-    .fadeOut(2000)
-    .next()
-    .fadeIn(2000)
-    .end()
-    .appendTo('#employees');
-},  5000);
+  setInterval(function() {
+    $('#employees > #table:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(3000)
+      .end()
+      .appendTo('#employees');
+  },  7000);
 })
 setInterval(myTimer, 100000);
 myTimer();
@@ -79,11 +69,9 @@ function myTimer() {
 
   $.getJSON("spData.json", function(json) {
 
-    var empLength = Object.keys(json).length;
-
+          var empLength = Object.keys(json).length;
           var table = document.createElement('table');
           table.setAttribute("id", "table");
-
 
           for (var k = 1; k < empLength + 1; k++) {
             var tableId = 1;
@@ -93,32 +81,24 @@ function myTimer() {
             var matches_array = officeNumber.match(/[0-9]{4}/g)
             if (matches_array){officeNumber = matches_array.pop()}
             if (isNaN(officeNumber)){officeNumber = ""}
-            if (officeNumber){
-
-
+            if (officeNumber) {
             var tr = document.createElement('tr');
-
             var td1 = document.createElement('td');
             var td2 = document.createElement('td');
             $(td2).attr("id","office");
-
             var empName = document.createTextNode(json[empString].name);
             var empOffice = document.createTextNode(officeNumber);
-
             td1.appendChild(empName);
             td2.appendChild(empOffice);
             tr.appendChild(td1);
             tr.appendChild(td2);
-
-
             table.appendChild(tr);
             if(k % 20 === 0 || k === empLength) {
               var newTables = document.getElementById("employees").appendChild(table);
               var table = document.createElement('table');
               table.setAttribute("id", "table");
-
             }
-          }
-    };
-  });
+        }
+    }
+  })
 }
