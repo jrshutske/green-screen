3e84cb7a13a82e8dc497e6af3e725390a5b528d0
Jack Shutske <jshutske@wisc.edu>
2018-08-07 12:47:59 -0500

    Implemented Event Grabber and changed names of files to make more sense

--- saveData.js
+++ saveData.js
@@ -1,13 +1,17 @@
-const spg = require('./spgrab');
+const getData = require('./getData');
 const express = require('express')
 const app = express();
 var http = require('http');
 var fs = require('fs');
 
 function saveData() {
-  fs.writeFile('spData.json', spg.getSharePointData(), function (err) {
+  fs.writeFile('spData.json', getData.getSharePointData().employeejson, function (err) {
     if (err) throw err;
   });
+  fs.writeFile('eventData.json', getData.getSharePointData().eventjson, function (err) {
+    if (err) throw err;
+  });
+  //Reading the file system and creating JSON file with all of the filepaths to images
   myfiles = {}
   myfiles.topleft = fs.readdirSync('./images/topleft');
   myfiles.bottomleft = fs.readdirSync('./images/bottomleft');
@@ -19,8 +23,9 @@ function saveData() {
   });
   console.log("New Data Saved");
 }
-//initial save
+//initial data save
 saveData();
+//timed data save
 setInterval(function(){
   saveData();
 }, 7200000);
