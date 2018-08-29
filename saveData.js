41e4f5669535e54f5dd641e30a610efa268d9b26
Brian Kyle <brian.kyle@wisc.edu>
2018-08-29 17:19:24 -0500

    use eslint to clean up js formatting

--- saveData.js
+++ saveData.js
@@ -4,22 +4,34 @@ var fs = require('fs');
 
 //writes JSON strings to .json files to be read later by application
 function saveData() {
-  fs.writeFile('./public/spData.json', getData.getSharePointData().employeejson, function (err) {
-    if (err) throw err;
-  });
-  fs.writeFile('./public/eventData.json', getData.getSharePointData().eventjson, function (err) {
-    if (err) throw err;
-  });
+  fs.writeFile(
+    './public/spData.json',
+    getData.getSharePointData().employeejson,
+    function(err) {
+      if (err) throw err;
+    },
+  );
+  fs.writeFile(
+    './public/eventData.json',
+    getData.getSharePointData().eventjson,
+    function(err) {
+      if (err) throw err;
+    },
+  );
   //Reading the file system and creating JSON file with all of the filepaths to images
-  myfiles = {}
+  myfiles = {};
   myfiles.topleft = fs.readdirSync('./public/images/topleft');
   myfiles.bottomleft = fs.readdirSync('./public/images/bottomleft');
   myfiles.topright = fs.readdirSync('./public/images/topright');
 
-  fs.writeFile('./public/images.json', JSON.stringify(myfiles, null, 4), function (err) {
-    if (err) throw err;
-  });
-  console.log("New Data Saved");
+  fs.writeFile(
+    './public/images.json',
+    JSON.stringify(myfiles, null, 4),
+    function(err) {
+      if (err) throw err;
+    },
+  );
+  console.log('New Data Saved');
 }
 //initial data save
 saveData();
